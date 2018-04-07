dd70e3a26c64ad449fe8717ddbf9a9933c17b2cc
Tero @RC-Thoughts.com <tero@rc-thoughts.com>
2018-04-07 12:00:49 +0300

    v1.0 Initial release

---
+++ README.md
@@ -0,0 +1,10 @@
+# LUA Randomizer
+Simple app tp play a random voicefile when model is selected or transmitter powered on
+
+Works in DC/DS-14/16/24 with firmware 4.22 and up
+
+Current info available on https://www.rc-thoughts.com/randomizer/
+
+Tero from RC-Thoughts 2018
+
+![Display-picture](https://cdn.rc-thoughts.com/wp-content/uploads/2018/04/RCT-Randomizer.png)
---
+++ Source/RCT-VRan.lua
@@ -0,0 +1,100 @@
+--[[
+	---------------------------------------------------------
+                    RCT Randomizer
+    
+    RCT Randomizer is a lua-app playing a random voicefile
+    from /Apps/StartVoices when model is selected or transmitter
+    is powered on. Includes a test-switch possibility.
+    
+    Works in DC/DS-14/16/24 with firmware 4.22 and up
+	---------------------------------------------------------
+	Localization-file has to be as /Apps/Lang/RCT-Wind.jsn
+    
+    Requires a folder /Apps/StartVoices to be added with some
+    audio-files in it, example:
+    
+    HelloWorld-file: /Apps/StartVoices/HelloWorld.wav
+    
+	---------------------------------------------------------
+	RCT Randomizer is part of RC-Thoughts Jeti Tools.
+	---------------------------------------------------------
+	Released under MIT-license by Tero @ RC-Thoughts.com 2018
+	---------------------------------------------------------
+--]]
+--------------------------------------------------------------------------------
+-- Locals for application
+local audioFiles, playDone, folder, testSw = {}, false, "Apps/StartVoices"
+--------------------------------------------------------------------------------
+-- Read and set translations
+local function setLanguage()
+    local lng=system.getLocale()
+    local file = io.readall("Apps/Lang/RCT-VRan.jsn")
+    local obj = json.decode(file)
+    if(obj) then
+        trans32 = obj[lng] or obj[obj.default]
+    end
+end
+--------------------------------------------------------------------------------
+function voiceDir()
+    for name, filetype in dir(""..folder.."") do
+        if(filetype == "file") then
+            local fullName = string.format("/%s/%s", folder, name)
+            table.insert(audioFiles, fullName)
+        end
+    end
+    --for key,value in pairs(audioFiles) do print(value) end
+end
+--------------------------------------------------------------------------------
+local function randomPlay()
+    system.playFile((audioFiles[math.random(#audioFiles)]),AUDIO_QUEUE)
+end
+----------------------------------------------------------------------
+-- Actions when settings changed
+local function testSwChanged(value)
+    local pSave = system.pSave
+	testSw = value
+	pSave("testSw", value)
+end
+--------------------------------------------------------------------------------
+-- Draw the main form (Application interface)
+local function initForm()
+    local form, addRow, addLabel = form, form.addRow ,form.addLabel
+    local addLink, addInputbox = form.addLink, form.addInputbox 
+    
+    addRow(1)
+    addLabel({label="---     RC-Thoughts Jeti Tools      ---",font=FONT_BIG})
+    
+    addRow(2)
+    addLabel({label=trans32.testSw, width=220})
+    addInputbox(testSw, true, testSwChanged)
+    
+    addRow(1)
+    addLabel({label="Powered by RC-Thoughts.com - v."..randomizerVersion.." ", font=FONT_MINI, alignRight=true})
+    collectgarbage()
+end
+--------------------------------------------------------------------------------
+local function loop()
+    testFile = system.getInputsVal(testSw)
+    if(testFile == 1 and not playDone) then  
+        randomPlay()
+        playDone = true
+    end
+    if(testFile ~= 1) then
+        playDone = false
+    end
+    collectgarbage()
+end
+--------------------------------------------------------------------------------
+local function init()
+    local pLoad, registerForm = system.pLoad, system.registerForm
+	testSw = pLoad("testSw")
+    registerForm(1, MENU_APPS, trans32.appName, initForm)
+    voiceDir()
+    randomPlay()
+    collectgarbage()
+end
+--------------------------------------------------------------------------------
+randomizerVersion = "1.0"
+setLanguage()
+collectgarbage()
+return {init=init, loop=loop, author="RC-Thoughts", version=randomizerVersion, name=trans32.appName}
